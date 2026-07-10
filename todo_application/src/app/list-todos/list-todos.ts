87cd2322fc40a59c2bfa312f166c56a81e3745c0
Gaurav Kulkarni <gaurav.somdatta.kulkarni@gmail.com>
2026-07-10 22:40:34 +0530

    Integrated api to get list of todos from backend and display in the view

--- todo_application/src/app/list-todos/list-todos.ts
+++ todo_application/src/app/list-todos/list-todos.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ChangeDetectorRef } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { DatePipe , UpperCasePipe,LowerCasePipe, NgFor } from '@angular/common';
+import { TodoData } from '../service/data/todo-data';
 
 export class Todo{
   constructor(
-    public todo_id:number,
+    public todoId:number,
+    public username:string,
     public description:string,
-    public done:boolean,
-    public target_date:Date
+    public addedDate:Date,
+    public targetDate:Date,
+    public done:boolean
   ){
 
   }
@@ -23,15 +26,28 @@ export class Todo{
 })
 export class ListTodos implements OnInit {
 
-  todos = [
-    new Todo(1, 'Learn Programming', false, new Date()),
-    new Todo(2, 'Become an Expert in Angular', false, new Date()),
-    new Todo(3, 'Visit America', false, new Date())
-  ];
+  // todos = [
+  //   new Todo(1, 'Learn Programming', false, new Date()),
+  //   new Todo(2, 'Become an Expert in Angular', false, new Date()),
+  //   new Todo(3, 'Visit America', false, new Date())
+  // ];
 
-  constructor() {}
+  todos: Todo[] = [];
+
+  constructor(
+    private todoService: TodoData,
+    private changeDetectorRef: ChangeDetectorRef
+  ) {}
 
   ngOnInit() {
     console.log('Component initialized');
+    this.todoService.getAllToDos('Gaurav').subscribe({
+      next: (response) => {
+        this.todos = response;
+        console.log('Todos fetched successfully:', this.todos);
+        this.changeDetectorRef.detectChanges();
+      }
+    });
+
   }
 }
---
+++ todo_application/src/app/service/data/todo-data.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Todo } from '../../list-todos/list-todos';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TodoData {
+  constructor(
+    private http: HttpClient
+  ){
+  }
+
+  getAllToDos(name: string) {
+      return this.http.get<Todo[]>(`http://localhost:8081/users/${name}/todos`);
+  }
+
+}
